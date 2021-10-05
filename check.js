2e6d8f9428e76aad80a22e02a6fc16fe72b42ea1
chasyumen <54601672+chasyumen@users.noreply.github.com>
2021-10-05 11:08:50 -0400

    Update check.js

--- check.js
+++ check.js
@@ -1,5 +1,7 @@
-var { WebServer } = require("./index.js");
+var { WebServer, GetFileType } = require("./index.js");
 var server = new WebServer({
   dir: "./",
   port: 3000
 });
+
+var filetype = GetFileType("/check.js")

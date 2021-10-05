3f2150677e01cd53b75b80f45273b51bba35106c
chasyumen <54601672+chasyumen@users.noreply.github.com>
2021-10-05 11:11:12 -0400

    Update check.js

--- check.js
+++ check.js
@@ -1,7 +1,11 @@
 var { WebServer, GetFileType } = require("./index.js");
 var server = new WebServer({
   dir: "./",
-  port: 3000
+  port: 3000,
+  acceptonlyget: true,
+  notfound: "This page shows that the url requested was not found on this server!",
+  req405error: "This page shows that you sent a request with incorrect method!"
 });
 
-var filetype = GetFileType("/check.js")
+var filename = "./check.js"
+var filetype = GetFileType(filename)

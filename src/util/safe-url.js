1b67642df6b4a7c7475de2511e20a799c52cb7dc
chasyumen <54601672+chasyumen@users.noreply.github.com>
2021-10-13 14:14:11 -0400

    Update safe-url.js

--- src/util/safe-url.js
+++ src/util/safe-url.js
@@ -7,13 +7,15 @@
  * const { SafeUrl } = require("lite-web-server");
  */
 
-function SafeUrl(url) {
+ function SafeUrl(url) {
     if (!typeof url == "string") {
         throw new Error("Invalid parameter specified.");
     }
     var _result = url.replace(/\.\./g, "").replace(/\/\//g, "");
     if (_result == "") {
         var result = "/";
+    } else if (!(_result.endsWith("/")&&url.endsWith("/"))) {
+        var result = _result+"/";
     } else {
         var result = _result;
     }

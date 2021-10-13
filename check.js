3723dfc5dcdf3f8196e46cf225b063fc825d185f
chasyumen <54601672+chasyumen@users.noreply.github.com>
2021-10-13 16:53:57 -0400

    Update check.js

--- check.js
+++ check.js
@@ -9,8 +9,6 @@ var server = new index.WebServer({
   dir: "./",
   port: 3000,
   acceptonlyget: true,
-  notfound: "This page shows that the url requested was not found on this server!",
-  req405error: "This page shows that you sent a request with incorrect method!",
   serveindex: true
 });
 

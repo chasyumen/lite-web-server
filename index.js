a06b8f7f80fcfc27a5fd343f709774d0d82e42e1
chasyumen <54601672+chasyumen@users.noreply.github.com>
2021-11-19 10:09:22 -0500

    for docs

--- index.js
+++
@@ -1,9 +0,0 @@
-/*
-    lite-web-server
-    Copyright (c) 2021 chasyumen
-    MIT Licensed
-*/
-
-'use strict'
-
-module.exports = require('./src');
--- src/mime-types/type-db.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-/*
-  Using mine-db files.
-
- */
-
-module.exports = require("./mime-db.1.50.0.min.json");
